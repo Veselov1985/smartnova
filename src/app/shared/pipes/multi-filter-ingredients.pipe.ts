8ce6756a87e2eab22003ab3e8281caaddcf95fcd
Moik <turangr@gmail.com>
2018-03-30 01:01:00 +0300

    ingredients multifilter fix

--- src/app/shared/pipes/multi-filter-ingredients.pipe.ts
+++ src/app/shared/pipes/multi-filter-ingredients.pipe.ts
@@ -18,33 +18,41 @@ export class MultiFilterIngredientsPipe implements PipeTransform {
       if (!regName.test(item.Name)) {
         return false;
       }
-      const issuance = +item.IssuanceVol.slice(0, item.IssuanceVol.indexOf(' '));
-      if (issuance < multifilter.IssuanceVolFrom && multifilter.IssuanceVolFrom) {
-        return false;
+      if (item.IssuanceVol) {
+        const issuance = item.IssuanceVol.match(/\d+/)[0];
+        if (issuance < multifilter.IssuanceVolFrom && multifilter.IssuanceVolFrom) {
+          return false;
+        }
+        if (issuance > multifilter.IssuanceVolTo && multifilter.IssuanceVolTo) {
+          return false;
+        }
       }
-      if (issuance > multifilter.IssuanceVolTo && multifilter.IssuanceVolTo) {
-        return false;
+      if (item.DownloadVol) {
+        const download = item.DownloadVol.match(/\d+/)[0];
+        if (download < multifilter.DownloadVolFrom && multifilter.DownloadVolFrom) {
+          return false;
+        }
+        if (download > multifilter.DownloadVolTo && multifilter.DownloadVolTo) {
+          return false;
+        }
       }
-      const download = +item.DownloadVol.slice(0, item.DownloadVol.indexOf(' '));
-      if (download < multifilter.DownloadVolFrom && multifilter.DownloadVolFrom) {
-        return false;
+      if (item.CurrentVol) {
+        const curVol = item.CurrentVol.match(/\d+/)[0];
+        if (curVol < multifilter.CurrentVolFrom && multifilter.CurrentVolFrom) {
+          return false;
+        }
+        if (curVol > multifilter.CurrentVolTo && multifilter.CurrentVolTo) {
+          return false;
+        }
       }
-      if (download > multifilter.DownloadVolTo && multifilter.DownloadVolTo) {
-        return false;
-      }
-      const curVol = +item.CurrentVol.slice(0, item.CurrentVol.indexOf(' '));
-      if (curVol < multifilter.CurrentVolFrom && multifilter.CurrentVolFrom) {
-        return false;
-      }
-      if (curVol > multifilter.CurrentVolTo && multifilter.CurrentVolTo) {
-        return false;
-      }
-      const threshold = +item.Threshold.slice(0, item.Threshold.indexOf(' '));
-      if (threshold < multifilter.ThresholdFrom && multifilter.ThresholdFrom) {
-        return false;
-      }
-      if (threshold > multifilter.ThresholdTo && multifilter.ThresholdTo) {
-        return false;
+      if (item.Threshold) {
+        const threshold = item.Threshold.match(/\d+/)[0];
+        if (threshold < multifilter.ThresholdFrom && multifilter.ThresholdFrom) {
+          return false;
+        }
+        if (threshold > multifilter.ThresholdTo && multifilter.ThresholdTo) {
+          return false;
+        }
       }
       return true;
     });
